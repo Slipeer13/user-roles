cd8391d29f7af113d16550ca58c42c3556f483c3
Slipeer <slipeer1@list.ru>
2022-09-30 16:35:46 +0500

    убрал @Component

--- src/main/java/ru/codemark/userroles/mapper/UserDTO.java
+++ src/main/java/ru/codemark/userroles/mapper/UserDTO.java
@@ -1,12 +1,10 @@
 package ru.codemark.userroles.mapper;
 
 import lombok.Data;
-import org.springframework.stereotype.Component;
 import ru.codemark.userroles.entity.Role;
 
 import java.util.List;
 
-@Component
 @Data
 public class UserDTO {
     private String name;
